5aa4e1105f30a453162f03c49c60b13993b95d0b
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2023-07-22 14:35:27 -0300

    Sistema de multa e verificador de nacionalidade

---
+++ AULA11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 72
+
+    console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+  if (vel>=60) {
+    console.log('Você ultrapassou a veocidade permitida. Você foi multado em R$100 reais')
+  }
+    console.log(`dirija sempre usando cinto de segrança`)
---
+++ AULA11/ex009.js
@@ -0,0 +1,9 @@
+var nome = 'augusto'
+var país = 'França'
+console.log(`Você esta vivendo em ${país}`)
+
+   if (país != 'Brasil') {
+      console.log('Você é Estrangeiro!')
+}  else {
+      console.log('Você é Brasileiro')
+}
